ff50a8dc0d44c3c7a2440fef40869b8019d56f55
sainathphad <phadsainath1@gmail.com>
2021-05-20 16:45:28 +0530

    ADDED: Start Position Of Player

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -3,5 +3,11 @@ package com.bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome To Snakes And Ladder Simulation");
+        //CONSTANTS
+        int START_POSITION = 0;
+
+        //VARIABLES
+        int player1 = START_POSITION;
+        int newPosition = 0;
     }
 }
