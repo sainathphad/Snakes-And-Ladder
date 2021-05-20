0e9d6c7a41ff3fe2e972238555cfbc37e689ddf4
sainathphad <phadsainath1@gmail.com>
2021-05-20 18:31:07 +0530

    ADDED: functionality to check options

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome To Snakes And Ladder Simulation");
@@ -7,11 +8,34 @@ public class SnakeAndLadder {
         int START_POSITION = 0;
 
         //VARIABLES
-        int player1 = START_POSITION;
         int newPosition = 0;
 
         //ROLLING_DIE
            int rollDie = 1 + (int)(Math.random() * 6);
             System.out.println("Dice: " +rollDie);
+        //OPTIONS
+        int optionCheck = 1 + (int)(Math.random() * 3);
+            switch (optionCheck) {
+                case 1:
+                    System.out.println("No Play");
+                    newPosition = START_POSITION;
+                    break;
+
+                case 2:
+                    newPosition = newPosition + rollDie;
+                    System.out.println("Ladder");
+                    break;
+
+                case 3:
+                    if (rollDie > newPosition) {
+                        newPosition = newPosition;
+                    }
+                    else {
+                        newPosition = newPosition - rollDie;
+                    }
+                    System.out.println("Snakes");
+                    break;
+            }
+                    System.out.println("New Position: " + newPosition);
     }
 }
